48fa3bcdc26f887c684a4c9280cba66f7384997f
david082321 <7046617+david082321@users.noreply.github.com>
2023-01-29 20:32:58 +0800

    Add zh_tw

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,4 +1,5 @@
 import zh_cn from './zh_cn.json'
+import zh_tw from './zh_tw.json'
 import en_us from './en_us.json'
 
 export default [
@@ -6,6 +7,11 @@ export default [
     id: 'zh_cn',
     name: '简体中文',
     translation: zh_cn,
+  },
+  {
+    id: 'zh_tw',
+    name: '繁體中文',
+    translation: zh_tw,
   },
   {
     id: 'en_us',
